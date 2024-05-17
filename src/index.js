6ccce407435d8cde0478560613428e0ef3c36de1
AlejandroDojo <ultiimately@gmail.com>
2024-05-17 11:19:48 -0400

    Reestructuración

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HolaMundo from './HolaMundo';
+import HolaMundo from './components/HolaMundo/HolaMundo';
 
 const contenedor = ReactDOM.createRoot(document.getElementById('myReactApp'));
 contenedor.render(
