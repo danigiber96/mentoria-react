1591c1a2f54dfdf10518cde4494105d406e0b517
AlejandroDojo <ultiimately@gmail.com>
2024-05-17 11:15:32 -0400

    Mentoría Viernes 17/05

---
+++ src/HolaMundo.jsx
@@ -0,0 +1,16 @@
+
+
+function HolaMundo() {
+  return (
+    <div>
+      <h1>Hola mundo!</h1>
+      <div> 
+        blabl ablablab lablalbalb lvbalbla lblablablba lbalabl balbal
+      </div>
+      <h2> Chau Mundo!</h2>
+      <h1>Mi app funcionando</h1>
+    </div>
+  );
+}
+
+export default HolaMundo;
